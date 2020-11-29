bd7e1d7e4a7a982563aafec927ebe9d991dc84aa
SergeM <sbmatyunin@gmail.com>
2020-11-29 11:30:30 +0100

    working jupyter starter

--- docker-guide/app/start_jup.sh
+++ docker-guide/app/start_jup.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-. `realpath ~/.bashrc`
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/intel/openvino/deployment_tools/open_model_zoo/tools/downloader:/opt/intel/openvino/deployment_tools/model_optimizer:~/.local/bin
+export PYTHONPATH=/opt/intel/openvino/data_processing/dl_streamer/python:/opt/intel/openvino/deployment_tools/model_optimizer:/opt/intel/openvino/deployment_tools/open_model_zoo/tools/downloader:/opt/intel/openvino/deployment_tools/open_model_zoo/tools/accuracy_checker:/opt/intel/openvino/python/python3:/opt/intel/openvino/python/python3.6:
+source /opt/intel/openvino/bin/setupvars.sh
 jupyter notebook --ip 0.0.0.0 --NotebookApp.password='sha1:618eee37fb55:6ba6f498317acdcd8657cf2c78116d2ec293795d' --no-browser
--- docker-guide/run.sh
+++ docker-guide/run.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-#docker run -it --rm -p 10003:8888 -v `realpath app`:/app openvino-jup:latest jupyter notebook --ip 0.0.0.0 --NotebookApp.password='sha1:618eee37fb55:6ba6f498317acdcd8657cf2c78116d2ec293795d' --no-browser
-docker run -it --rm -p 10003:8888 -v `realpath app`:/app --name jup openvino-jup:latest sh /app/start_jup.sh
+docker run -it --rm -p 10003:8888 --user `id -u`:`id -g` -v `realpath app`:/app openvino-jup:latest /app/start_jup.sh
 
