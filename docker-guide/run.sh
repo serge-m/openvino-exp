25495bcc84340f34857327977bd8af19ec7bd030
SergeM <sbmatyunin@gmail.com>
2020-11-29 12:03:50 +0100

    run jupyter docker in background

--- docker-guide/run.sh
+++ docker-guide/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run -it --rm -p 10003:8888 --user `id -u`:`id -g` -v `realpath app`:/app openvino-jup:latest /app/start_jup.sh
+docker run --rm -p 10003:8888 --user `id -u`:`id -g` -v `realpath app`:/app --name openvino_jup openvino-jup:latest /app/start_jup.sh
 
